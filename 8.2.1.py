af560486c1c0c38c63187275524b8954eed9e40e
Dimitriy <dimadjamalov34@gmail.com>
2024-11-05 20:58:10 +0300

    Практика 8

--- 8.2.1.py
+++ 8.2.1.py
@@ -1,18 +1,18 @@
 n = int(input()) #кол-во столбцов
-li = []
+s = []
 flag = 0
 for i in range(n):
-    li.append([int(x) for x in input().split()]) #числа в столбцах
-for k in range(len(li) - 1):
-    if sum(li[k]) == sum(li[k + 1]): #проверка по строчкам
-        res = []
+    s.append([int(x) for x in input().split()]) #числа в столбцах
+for k in range(len(s) - 1):
+    if sum(s[k]) == sum(s[k + 1]): #проверка по строчкам
+        result = []
         for j in range(n): #диапозон от 0 до n
-            pr = []
-            for i in range(len(li)): #список строк
-                pr.append(li[i][j]) #столбцы
-            res.append(pr) #для проверки
-        for i in range(len(res) - 1):
-            if sum(res[i]) == sum(res[i + 1]): #проверка по столбам
+            y = []
+            for i in range(len(s)): #список строк
+                y.append(s[i][j]) #столбцы
+            result.append(y) #для проверки
+        for i in range(len(result) - 1):
+            if sum(result[i]) == sum(result[i + 1]): #проверка по столбам
                 flag = 1
             else:
                 flag = 0
