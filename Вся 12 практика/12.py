d84d8506706dc460eacf526d56d4cba25a702417
Dmitriy-IB <dzhamalov06@bk.ru>
2024-12-04 01:15:03 +0300

    Вся 12 практика

--- Вся 12 практика/12.py
+++ Вся 12 практика/12.py
@@ -1,10 +1,11 @@
 # Задание: даны самые популярные репозитории на github, по последней цифре зачетки получить JSON для вашего варианта.
 # Программа с графическим интерфейсом вводим в поле имя репозитория и по нажатию кнопки получаем результат.
 # Необходимо получить в новый файл следующую информацию:'company' 'created_at' 'email' 'id' 'name' 'url'
-# Вcе прикрепить одним архивом.
+# Всё прикрепить одним архивом.
 from tkinter import *
 import json
 import requests
+from tkinter import messagebox
 
 def polychit():
     username = entry.get() # вводим имя репрезитория
@@ -22,16 +23,20 @@ def polychit():
             'name': (zagruzka['name']),
             'url': (zagruzka['url'])
         } # необходимая часть данных (ключей), полученных из API GitHub
-        with open('dannie.json', 'w', ) as vivod:
-            json.dump(info, vivod, indent=4) # перезапись json файла только с нужными ключами с 4-мя пробелами
+        with open('dannie.json', 'w',) as vivod:
+            json.dump(klyuchi, vivod, indent=4) # перезапись json файла только с нужными ключами с 4-мя пробелами
+            messagebox.showinfo('Результат', 'Информация успешно выведена в файл "dannie.json"')
 
 root = Tk()
 root.title('Джамалов Дмитрий Максимович')
 root.geometry('400x300+750+375')
 root.resizable(False, False)
+root.config(bg='#E8E116')
 entry = Entry(root) # поле для ввода репрезитория
+entry.config(fg='#9100FF')
 entry.pack()
-but = Button(root, text='Вывести информацию', command=polychit) #
+but = Button(root, text='Вывести информацию', command=polychit)
+but.config(bg='#C6C02A')
 but.pack()
 
 root.mainloop()
